4170d2c7e78dd92a8553bcda4fb9e73d7cbd08aa
Tay Ray Chuan <rctay89@gmail.com>
2010-01-09 17:40:03 +0800

    strip out 'self' prefix for products

--- tests.py
+++ tests.py
@@ -21,24 +21,24 @@ class LocalTestCaseNormal(unittest.TestCase):
     def setUp(self):
         self.site = Site.objects.get_current()
 
-        self.p1 = Product.objects.create(
+    def test_shipping(self):
+        cart1 = Cart.objects.create(site=self.site)
+        cart2 = Cart.objects.create(site=self.site)
+
+        p1 = Product.objects.create(
             site=self.site,
             name='Shoulder Blouse',
             slug='shoulder-blouse',
             items_in_stock=10,
             weight='315', weight_units='gms')
 
-    def test_shipping(self):
-        cart1 = Cart.objects.create(site=self.site)
-        cart2 = Cart.objects.create(site=self.site)
-
-        cart1.add_item(self.p1, 1)
-        cart2.add_item(self.p1, 3)
+        cart1.add_item(p1, 1)
+        cart2.add_item(p1, 3)
 
         ship1 = singpost(cart1, None)
         ship2 = singpost(cart2, None)
 
-        self.assertTrue(self.p1.is_shippable)
+        self.assertTrue(p1.is_shippable)
 
         self.assertTrue(cart1.is_shippable)
         self.assertEqual(ship1._weight(), Decimal('315'))
@@ -52,24 +52,24 @@ class LocalTestCaseHeavy(unittest.TestCase):
     def setUp(self):
         self.site = Site.objects.get_current()
 
-        self.p1 = Product.objects.create(
+    def test_shipping1(self):
+        p1 = Product.objects.create(
             site=self.site,
             name='Shoulder Blouse2',
             slug='shoulder-blouse2',
             items_in_stock=10,
             weight='315', weight_units='gms')
 
-        self.p2 = Product.objects.create(
+        p2 = Product.objects.create(
             site=self.site,
             name='Shoulder Blouse3',
             slug='shoulder-blouse3',
             items_in_stock=10,
             weight='200', weight_units='gms')
 
-    def test_shipping1(self):
         # should split into 2 shipments: [6, 3]
         cart1 = Cart.objects.create(site=self.site)
-        cart1.add_item(self.p1, 9)
+        cart1.add_item(p1, 9)
         ship1 = singpost(cart1, None)
         self.assertTrue(cart1.is_shippable)
         self.assertEqual(ship1._weight(), Decimal('2835'))
@@ -77,8 +77,8 @@ class LocalTestCaseHeavy(unittest.TestCase):
 
         # should split into 2 shipments: [6, 3p1+5p2, 1p2]
         cart2 = Cart.objects.create(site=self.site)
-        cart2.add_item(self.p1, 9)
-        cart2.add_item(self.p2, 6)
+        cart2.add_item(p1, 9)
+        cart2.add_item(p2, 6)
         ship2 = singpost(cart2, None)
         self.assertTrue(cart2.is_shippable)
         self.assertEqual(ship2._weight(), Decimal('4035'))
@@ -102,16 +102,16 @@ class LocalTestCaseLight(unittest.TestCase):
     def setUp(self):
         self.site = Site.objects.get_current()
 
-        self.p1 = Product.objects.create(
+    def test_shipping1(self):
+        p1 = Product.objects.create(
             site=self.site,
             name='Stipple Sponge',
             slug='stipple-sponge',
             items_in_stock=10,
             weight='1.6', weight_units='gms')
 
-    def test_shipping1(self):
         cart1 = Cart.objects.create(site=self.site)
-        cart1.add_item(self.p1, 1)
+        cart1.add_item(p1, 1)
         ship1 = singpost(cart1, None)
         self.assertTrue(cart1.is_shippable)
         self.assertEqual(ship1._weight(), Decimal('1.6'))
