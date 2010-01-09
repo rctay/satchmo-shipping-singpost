2745f95d9e636f2a378cd2c90c5238883525d5f3
Tay Ray Chuan <rctay89@gmail.com>
2010-01-09 20:27:40 +0800

    refactor shipment partitioning
    The weight-cost class should implement it for us.

--- shipper.py
+++ shipper.py
@@ -32,6 +32,12 @@ class BaseWeightCostMap:
     def cost_for_weight(self, total_weight):
         raise NotImplementedError
 
+    """
+    Returns a list of shipments.
+    """
+    def partitioned_shipments(self, total_weight, cart):
+        raise NotImplementedError
+
 class TieredWeightCostMap(BaseWeightCostMap):
     """
     The weight of a single must fall within specified "tiers", therefore the
@@ -52,6 +58,45 @@ class TieredWeightCostMap(BaseWeightCostMap):
                 prev = weight
 
         return result_cost
+
+    def partitioned_shipments(self, total_weight, cart):
+        max_weight = self.get_heaviest_weight()
+
+        shipments = []
+        a_shipment = []
+
+        if total_weight < max_weight:
+            # optimized version - no need to check weight for every item
+            for cartitem in cart.cartitem_set.all():
+                for i in xrange(cartitem.quantity):
+                    a_shipment.append(cartitem)
+        else:
+            the_weight = Decimal('0')
+            new_weight = None
+            for cartitem in cart.cartitem_set.all():
+                for i in xrange(cartitem.quantity):
+                    new_weight = the_weight + Decimal(cartitem.product.weight)
+
+                    if new_weight <= max_weight:
+                        the_weight = new_weight
+                        a_shipment.append(cartitem)
+
+                        if new_weight == max_weight:
+                            shipments.append(a_shipment)
+                            a_shipment = []
+                    elif len(a_shipment) > 0 and the_weight > Decimal('0'):
+                        shipments.append(a_shipment)
+                        a_shipment = [cartitem]
+                        the_weight = Decimal(cartitem.product.weight)
+                    else:
+                        log.error("item exceeds max weight: name=%s, weight=%d" \
+                            % (cartitem.product.name, cartitem.product.weight))
+                        return None
+
+        if len(a_shipment):
+            shipments.append(a_shipment)
+
+        return shipments
 
 WEIGHT_COST_MAPS = {
     'LOCAL': TieredWeightCostMap((
@@ -102,48 +147,6 @@ class Shipper(BaseShipper):
 
         return total_weight
 
-    """
-    Returns a list of shipments.
-    """
-    def _partitioned_shipments(self, wcm):
-        max_weight = wcm.get_heaviest_weight()
-
-        shipments = []
-        a_shipment = []
-
-        if self._weight() < max_weight:
-            # optimized version - no need to check weight for every item
-            for cartitem in self.cart.cartitem_set.all():
-                for i in xrange(cartitem.quantity):
-                    a_shipment.append(cartitem)
-        else:
-            the_weight = Decimal('0')
-            new_weight = None
-            for cartitem in self.cart.cartitem_set.all():
-                for i in xrange(cartitem.quantity):
-                    new_weight = the_weight + Decimal(cartitem.product.weight)
-
-                    if new_weight <= max_weight:
-                        the_weight = new_weight
-                        a_shipment.append(cartitem)
-
-                        if new_weight == max_weight:
-                            shipments.append(a_shipment)
-                            a_shipment = []
-                    elif len(a_shipment) > 0 and the_weight > Decimal('0'):
-                        shipments.append(a_shipment)
-                        a_shipment = [cartitem]
-                        the_weight = Decimal(cartitem.product.weight)
-                    else:
-                        log.error("item exceeds max weight: name=%s, weight=%d" \
-                            % (cartitem.product.name, cartitem.product.weight))
-                        return None
-
-        if len(a_shipment):
-            shipments.append(a_shipment)
-
-        return shipments
-
     def _cost_for_shipment(self, shipment, wcm):
         total_weight = self._weight_for_shipment(shipment)
 
@@ -163,7 +166,7 @@ class Shipper(BaseShipper):
 
         wcm = WEIGHT_COST_MAPS[self.service_type]
 
-        shipments = self._partitioned_shipments(wcm)
+        shipments = wcm.partitioned_shipments(self._weight(), self.cart)
 
         if shipments == None:
             return None
